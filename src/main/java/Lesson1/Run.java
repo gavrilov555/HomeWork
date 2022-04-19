b3249c21ff3e94fce2fc5a61022ef0c42a25a486
gavrilov_v <gavrilov_v92@mail.ru>
2022-04-19 22:28:55 +0500

    add Run

--- src/main/java/Lesson1/Run.java
+++ src/main/java/Lesson1/Run.java
@@ -1,5 +1,20 @@
 package Lesson1;
 
 public class Run {
+    public static void main(String[] args) {
 
+        Director director = new Director();
+
+        director.setBuilder(new OnePerson());
+        Person person = director.buildPerson();
+        System.out.println(person);
+
+        System.out.println("=============================================");
+
+        director.setBuilder(new TwoPerson());
+        Person person2 = director.buildPerson();
+        System.out.println(person2);
+
+
+    }
 }
