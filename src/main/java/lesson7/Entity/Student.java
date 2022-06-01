b3d6da6f11b4f649903e1fb6196f1e5a37e83480
gavrilov555 <gavrilov_v92@mail.ru>
2022-06-01 10:53:15 +0500

    lesson7_ add database postgres, edited "yaml" file and run class (SpringWebApp).

--- src/main/java/lesson7/Entity/Student.java
+++ src/main/java/lesson7/Entity/Student.java
@@ -16,12 +16,9 @@ public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "age")
     private int age;
 }
--- src/main/java/lesson7/SpringWebApp.java
+++ src/main/java/lesson7/SpringWebApp.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringWebApp {
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringApplication.class, args);
+        SpringApplication.run(SpringWebApp.class, args);
     }
 }
